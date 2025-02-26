5d35e3517f96028951f591e3b22e9b041ff7ae87
MandarShukla12 <85058620+MandarShukla12@users.noreply.github.com>
2025-02-26 23:04:40 +0530

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -3,57 +3,76 @@
 #include <stdlib.h>
 #include "lexer.h"
 
-struct twinBuffer B;
-int cno=0;
-int lno=0;
+twinBuffer* buffer;
+
+
 //running count of line and character, so as to populate the fields in the tokenInfo structure in getNextToken(twinBuffer B) 
 
 
 void removeComments(char *testcaseFile, char *cleanFile){//assumes char array terminates with \0, and returns a char array terminated with \0 as well
-    free(cleanFile);
+    FILE* tcf=fopen(testcaseFile,"r");
+    FILE*  cf=fopen(cleanFile,"w");
 
-    cleanFile=malloc((strlen(testcaseFile)+1)*sizeof(char));
-    int i=0,j=0;
-    while(testcaseFile[i]!='\0'){
-        if(testcaseFile[i]=='%'){
-            while((testcaseFile[i]!='\0')&&(testcaseFile[i]!='\n')){i++;}
-        }
-        cleanFile[j]=testcaseFile[i];
-        i++;j++;
+    if(tcf==NULL||cf==NULL){
+        printf("Error: file %s or %s not found in directory",testcaseFile,cleanFile);
     }
-    cleanFile[j]='\0';
+    
+    int flag=0;
+    char temp=fgetc(tcf);
+    while(temp!=EOF){
+        if(temp=='%'){
+           flag=1;
+        }
+        if((flag==1)&&(temp=='\n')){flag=0;}
+        else if(!flag) fputc(temp,cf);
+        temp=fgetc(tcf);
+    }
+    fclose(tcf);
 }
 
 FILE *getStream(FILE *fp) {//populates twin buffer(a global variable) with 200 characters  
-    fgets(B.buf1,100,fp);
-    fgets(B.buf2,100,fp);
+    if (fp == NULL) {
+        printf("Error: File pointer is NULL\n");
+        return NULL;
+    }
+    if (buffer->currbuf==0){
+        fread(buffer->buf2, sizeof(char), 30, fp);
+        buffer->index=0;
+    }
+    if (buffer->currbuf==1){
+        fread(buffer->buf1, sizeof(char), 30, fp);
+        buffer->index=0;
+    }
     return fp;
 }
 
-//simple function that converts a string to an integer
-int stringToInt(const char *str) {
-    int result = 0;
-    int sign = 1;
-    
+tokenInfo getNextToken(twinBuffer B){
+    tokenInfo token;
+    lexeme lex;
+    int state=0;
+    while (1){
+        switch (state){
+            case 
+        }
 
-    if (*str == '-') {
-        sign = -1;
-        str++; 
     }
+}
 
-    while (*str >= '0' && *str <= '9') {
-        result = result * 10 + (*str - '0');
-        str++;
-    }
-
-    return result * sign;
+int main(){
+    buffer=(twinBuffer *)malloc(sizeof(twinBuffer));
+    buffer->lno=1;
+    buffer->cno=1;
+    buffer->index=0;
+    buffer->currbuf=1;
+    FILE* test=fopen("test.txt","r");
+    test=getStream(test);
+    printf("Buffer 1: %s\n", buffer->buf1);
+    printf("Buffer 2: %s\n", buffer->buf2);
+    printf("Current Buffer: %d\n", buffer->currbuf);
+    printf("Index: %d\n", buffer->index);
+    printf("Character Number: %d\n", buffer->cno);
+    printf("Line Number: %d\n", buffer->lno);
+    return 0;
 }
 
 
-/*tokenInfo getNextToken(twinBuffer B){
-
-}*/
-
-void main(){
-    printf("hello");
-}
