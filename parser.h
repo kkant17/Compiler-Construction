04b16f366bc1f4ca8491e976c0cf2d768dc85c30
kkant17 <118473890+kkant17@users.noreply.github.com>
2025-03-13 20:55:42 +0530

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,6 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
-
+#include <stdbool.h>
 #include "lexer.h"
 
 
@@ -86,21 +86,66 @@ typedef struct {
 } FirstFollowSet;
 
 // Define a structure for storing the parse table cells
+typedef struct {
+    bool rulePresent;  // Is there a rule in this cell?
+    bool error;
+    bool syn;
+    GrammarRule* rule;  // Only valid if type == PT_ENTRY_RULE
+} ParseTableCell;
 typedef struct{
-    GrammarRule** rule;
-    GrammarRule*** rule;
+    ParseTableCell*** cells;
 }ParseTable;
+
+typedef struct ParseTreeNode {
+    Token symbol;
+    union {
+        struct {
+            struct ParseTreeNode** children;
+            int child_count;
+        } non_terminal;          
+        struct {
+            char* lexeme;
+            int line_number;
+        } terminal;
+    } node;  // Named union member
+} ParseTreeNode;
+
+typedef struct {
+    bool isTerminal;
+    union {
+        NonTerminal nt;
+        tk terminal;
+    } symbol;
+    ParseTreeNode* parent;
+} StackEntry;
+
+typedef struct {
+    StackEntry* items;
+    int top;
+    int capacity;
+} Stack;
 
 // Function prototypes
 void storeGrammarRules();
-FirstFollowSet* storeFirstFollowSets();
-FirstFollowSet* ComputeFirstFollowSets()
-void createParseTable(FirstFollowSet* F, ParseTable* T)
-#endif
-=======
-FirstFollowSet* computeFirstFollowSets();
+void printGrammarRules();
+// void printFirstSets();
+FirstFollowSet* ComputeFirstFollowSets();
 void createParseTable(FirstFollowSet* F, ParseTable** T);
-void freeParseTable(ParseTable* T);
+// void freeParseTable(ParseTable* T);
 int containsEpsilon(tk* firstSet, int count);
+// void getRuleString(GrammarRule* rule, char* buffer, const char** nonTerminalNames, const char** terminalNames);
+void printParseTable(ParseTable* T, const char** nonTerminalNames, const char** terminalNames) ;
+ParseTreeNode* create_non_terminal_node(NonTerminal nt);
+ParseTreeNode* create_terminal_node(tk token_type, const char* lexeme, int line);
+void add_child(ParseTreeNode* parent, ParseTreeNode* child);
+void free_parse_tree(ParseTreeNode* root);
+void print_parse_tree(const ParseTreeNode* node, int indent_level, const char** non_term_names, const char** term_names);
+Stack* createStack(int capacity);
+void push(Stack* stack, StackEntry entry);
+StackEntry pop(Stack* stack);
+int isEmpty(Stack* stack);
+StackEntry top(Stack* stack);
+void freeStack(Stack* stack);
+int isInFirstSet(FirstFollowSet* F, NonTerminal nt, tk token)
 
 #endif
