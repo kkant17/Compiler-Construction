d852d2d7dbb11b43ad79d6ac240cf305e9613159
dbsprojbanayenga <165247703+dbsprojbanayenga@users.noreply.github.com>
2025-03-15 00:19:32 +0530

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -7,4 +7,5 @@ void removeComments(char *testcaseFile, char *cleanFile);
 FILE *getStream(twinBuffer* buffer, FILE *fp, int bufno);
 tokenInfo getNextToken(twinBuffer* B, FILE *fp, lookuptbl* table);
 
+extern const char* terminalNames[MAX_TERMINALS];
 #endif
